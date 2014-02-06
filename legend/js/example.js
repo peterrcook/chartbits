ec58a779ab5c6048431c2ac7b494dfb8721cc0c8
Peter Cook <peter@prcweb.co.uk>
2014-02-06 16:39:50 +0000

    Legend interaction

--- legend/js/example.js
+++ legend/js/example.js
@@ -12,22 +12,27 @@
     }
   ];
 
-  var dispatch = d3.dispatch('legendClick');
+  var dispatch = d3.dispatch('legendClick', 'legendChange');
 
    // Construction
   function construct1() {
     var legend = animdata.d3.legend()
       .size(12)
       .dispatch(dispatch)
-      .clickEvent('legendClick');
+      .status([true, false])
+      .clickEvent('legendClick')
+      .changeEvent('legendChange');
 
     d3.select('#charts .chart1 .legend')
       .datum(legendData)
       .call(legend);
 
     dispatch.on('legendClick', function(id) {
+      alert('click', id);
+    });
+    dispatch.on('legendChange', function(status) {
       d3.select('#charts .chart1 .info')
-        .text("You've selected " + id);
+        .text("Status now " + status.join(' '));
     });
   }
 
--- legend/js/legend.js
+++ legend/js/legend.js
@@ -16,7 +16,9 @@ animdata.d3.legend = function() {
   var config = {
     size: 14,
     dispatch: null,
-    clickEvent: ''
+    clickEvent: '',
+    changeEvent: '',
+    status: null
   }
 
 
@@ -32,12 +34,21 @@ animdata.d3.legend = function() {
     container: null,
   }
 
+  var status = [];
 
-  /*----
-  Chart building
-  ----*/
-  function construct() {
+  /*--
+  Init
+  --*/
+  function init() {
+    if(!config.status) {
+      config.status = _.map(data, function(d) {
+        return true;
+      });
+    }
+    status = config.status;
+    // console.log(config.status);
   }
+
 
 
 
@@ -52,22 +63,56 @@ animdata.d3.legend = function() {
     u.enter()
       .append('div')
       .classed('item', true)
+      .classed('selected', function(d, i) {
+        return status[i];
+      })
       .style('float', 'left')
-      .style('border-left', function(d) {
+      .style('border-left', function(d, i) {
         var ret = config.size + 'px ';
         ret += 'solid ';
-        ret += d.color;
         return ret;
       })
       .style('font-size', config.size + 'px')
       .style('padding-left', '5px')
       .style('margin-right', '15px')
       .style('cursor', 'pointer')
-      .on('click', function(d) {
-        config.dispatch[config.clickEvent](d.id);
-      });
+      .on('click', function(d, i) {
+        var e = d3.select(this);
+        var selected = e.classed('selected');
+        e.classed('selected', !selected);
+        status[i] = !selected;
+        updateColors();
+        if(config.clickEvent)
+          config.dispatch[config.clickEvent](d.id);
+        if(config.changeEvent)
+        config.dispatch[config.changeEvent](status);
+      })
+      .each(noSelectCSS);
 
     u.text(function(d) {return d.label;});
+
+    updateColors();
+  }
+
+  function noSelectCSS() {
+    d3.select(this)
+      .style({
+        '-webkit-touch-callout': 'none',
+        '-webkit-user-select': 'none',
+        '-khtml-user-select': 'none',
+        '-moz-user-select': 'none',
+        '-ms-user-select': 'none',
+        'user-select': 'none'
+      });
+  }
+
+  function updateColors() {
+    d3elements.container
+      .selectAll('div.item')
+      .style('border-left-color', function(d, i) {
+        var color = status[i] ? d.color : '#ddd';
+        return color;
+      });
   }
 
 
@@ -86,7 +131,7 @@ animdata.d3.legend = function() {
 
     // Construct the chart if this is first call
     if(!constructed) {
-      construct();
+      init();
     }
 
     update();
